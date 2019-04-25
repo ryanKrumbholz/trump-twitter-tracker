4efa2d10634524ee6f811f0ce659e6e537aa7e15
Ryan Krumbholz <krumbholz98@gmail.com>
2019-04-25 18:33:43 -0400

    Update my_twitter_bot.py

--- my_twitter_bot.py
+++ my_twitter_bot.py
@@ -9,7 +9,7 @@ ACCESS_SECRET = 'your key' #twitter developer secret
 
 auth =  tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET) #creating the authentication
 auth.set_access_token(ACCESS_KEY,ACCESS_SECRET)
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
 def exit():
     print("Exiting program")
