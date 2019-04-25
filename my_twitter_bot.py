47e290e30e42d255a16ddcbeffa2f367f100efb2
Ryan Krumbholz <krumbholz98@gmail.com>
2019-04-25 16:39:58 -0400

    Update my_twitter_bot.py

--- my_twitter_bot.py
+++ my_twitter_bot.py
@@ -11,11 +11,15 @@ auth =  tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET) #creating the authent
 auth.set_access_token(ACCESS_KEY,ACCESS_SECRET)
 api = tweepy.API(auth)
 
+def exit():
+    print("Exiting program")
+    SystemExit()
+
 def write_last_id(id): #writes mention tweet id to txt file to prevent duplicate responses
     f = open('uniqueIDs.txt','w')
     f.write(str(id))
     f.close()
-    
+
 def read_last_id(): #reads from txt file to prevent duplicate responses
     f = open('uniqueIDs.txt','r')
     lastid = int(f.read().strip())
@@ -23,31 +27,31 @@ def read_last_id(): #reads from txt file to prevent duplicate responses
     return lastid
 
 def count_tweets():
-    tweets = api.user_timeline('@realDonaldTrump') #tweets from Donald Trump's twitter
+    tweets = api.user_timeline('@realDonaldTrump', include_rts=False) #tweets from Donald Trump's twitter
     last_seen_id = read_last_id()
     mentions = api.mentions_timeline(last_seen_id) #only mentions that haven't been seen yet
     count = 0 #number of tweets per day
-    today = time.localtime()[2] #today's day of the month (numerically)
-    
+    today = time.gmtime()[2] #today's day of the month (numerically)
+
     def tweet_oncommand():
-        current_time = str(time.localtime()[3]) + 'hrs ' + str(time.localtime()[4]) + 'mins ' + str(time.localtime()[5]) + 'secs'
+        current_time = str(time.gmtime()[3]) + 'hrs ' + str(time.gmtime()[4]) + 'mins ' + str(time.gmtime()[5]) + 'secs'
         for mention in reversed(mentions): #iterating through all unseen mentions
             user = mention.user.screen_name #twitter username
             last_seen_id = mention.id #updating last_seen_id
             if '#count' in mention.text:
                 print("Count command made")
-                api.update_status("@" + user + " Trump has tweeted " + str(count) + " times so far today as of " + current_time + "! (including retweets)")
+                api.update_status("@" + user + " Trump has tweeted " + str(count) + " times so far today as of " + current_time + "!")
                 write_last_id(str(last_seen_id))
                 print("Status updated")
 
     def tweet_count_midnight():
         yesterday = datetime.date.today() - datetime.timedelta(1)
-        midnight = time.localtime()[3] == 0 and time.localtime()[4] == 0 and time.localtime()[5] <= 15 #bool that is true when it is midnight
+        midnight = time.gmtime()[3] == 0 and time.gmtime()[4] == 0 and time.gmtime()[5] <= 15 #bool that is true when it is midnight
         if midnight:
             print("Tweeting midnight status")
-            api.update_status("Yesterday (" + yesterday.strftime('%m/%d/%Y') + ") Trump tweeted " + str(count) + " times! (including retweets)")
+            api.update_status("Yesterday (" + yesterday.strftime('%m/%d/%Y') + ") Trump tweeted " + str(count) + " times!")
             print("Status tweeted")
-    
+
     for tweet in tweets: #iterates through DT tweets
         if tweet.created_at.timetuple()[2] == today: #if the tweet was made today then update the count
             count +=1
@@ -60,3 +64,4 @@ def count_tweets():
 while True:
     count_tweets()
     time.sleep(15)
+
